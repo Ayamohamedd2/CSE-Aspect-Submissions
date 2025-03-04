cb10cb2b1c7e0543d1e6d1927ca7853bb10056ca
Ayamohamedd2 <20P7066@eng.asu.edu.eg>
2025-03-04 19:58:47 +0200

    lab-01 done

---
+++ lab-01/src/main/java/com/example/demo/Controlllers/Controller.java
@@ -0,0 +1,31 @@
+package com.example.demo.Controlllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/data")
+public class Controller {
+
+    @GetMapping
+    public String getData() {
+        return "GET response";
+    }
+
+    @PostMapping
+    public String postData() {
+        return "POST response";
+    }
+
+    @PutMapping
+    public String putData() {
+        return "PUT response";
+    }
+
+    @DeleteMapping
+    public String deleteData() {
+        return "DELETE response";
+    }
+
+
+}
+
